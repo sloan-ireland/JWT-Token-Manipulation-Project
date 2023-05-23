6c5fb23a320b7d6e5f8e7b2d13762781b9c4a218
jsk11235 <kirmayerjacob@gmail.com>
2023-05-23 11:28:14 -0400

    server code pushed

--- website/server.js
+++ website/server.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const jwt  = require('jsonwebtoken')
 
 app.use(express.urlencoded());
 app.use(bodyParser.json({limit: '10mb', extended: true}));
 app.post('/login',async (req,res)=>{
   console.log(req.body)
-  res.json(req.headers)
+  res.json(jwt.sign({user:req.body.username},'readthisfromfilelater'))
 })
 
 app.listen(8080,()=>{console.log('rdy')})
