f0cfdc89cd14b8b0106eb0231dd8fd4bd5aa3000
Sloan <sireland30@stuy.edu>
2023-05-25 14:36:10 +0000

    server code changes so js code can pull the flag

--- website/server.js
+++ website/server.js
@@ -3,6 +3,7 @@ const app = express()
 const bodyParser = require('body-parser')
 const jwt  = require('jsonwebtoken')
 const cors = require('cors')
+const fs = require('fs')
 
 app.use(cors());
 app.use(express.urlencoded({extended:true}));
@@ -12,19 +13,20 @@ app.use(bodyParser.json({limit: '10mb',extended:true}));
 app.post('/login',async (req,res)=>{
   console.log(req.body)
   if (req.body.password === 'jimmy') {
-    res.json(jwt.sign({username: req.body.username}, 'readthisfromfilelater'))
+    		console.log(jwt.sign({username: req.body.username}, 'readthisfromfilelater'))
+	  res.json(jwt.sign({username: req.body.username}, 'readthisfromfilelater'))
   }
 })
 
 app.post('/getFlag',async (req,res)=>{
-  const verified = jwt.verify(req.headers.token,'readthisfromfilelater',{
+  const verified = jwt.verify(req.body.token,'readthisfromfilelater',{
     algorithms:['HS256','HS384','HS512','RS256','RS384','none']
   })
   if (verified.username==='jimmy'){
-    res.json('flag1: ')
+    res.json('flag1: ' + fs.readFileSync('../flags/flag1'))
   }
   if (verified.username==='administrator') {
-    res.json('flag2')
+    res.json('flag2: ' + fs.readFileSync('../flags/flag2'))
   }
 })
 
