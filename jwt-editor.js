b3b633d92035d2f4f3d8604470996237f7a0a685
jsk11235 <kirmayerjacob@gmail.com>
2023-05-25 17:22:03 -0400

    rewrote jwt editor with stacks and promises

--- jwt-editor.js
+++ jwt-editor.js
@@ -7,6 +7,14 @@
 const jwt = require("jsonwebtoken");
 const readline = require("readline");
 
+async function getInput(prompt) {
+  const rl = readline.createInterface(process.stdin, process.stdout);
+  return new Promise((resolve, reject) => rl.question(prompt, (token) => {
+    rl.close()
+    resolve(token)
+  }))
+}
+
 // Get the token from the command line
 const token = process.argv[2];
 if (!token) {
@@ -21,63 +29,59 @@ if (!payload) {
   process.exit(1);
 }
 
-// Create a readline interface
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
 
+
+(async () => {
 // Define a function to prompt the user for changes
-const prompt = (path, obj) => {
-  // Display the current object at the path
-  console.log(`Current payload at ${path}:`);
-  console.log(JSON.stringify(obj, null, 2));
-
-  // Ask the user what they want to change
-  rl.question(
-    "Enter a key to change its value, or 'cd' to navigate to a nested object, or 'save' to save and exit: ",
-    (answer) => {
-      // If the user enters 'save', encode the payload and print the new token
-      if (answer === "save") {
-        const newToken = jwt.sign(payload, "secret");
-        console.log(`New JWT token: ${newToken}`);
-        rl.close();
-      }
-      // If the user enters 'cd', ask them for the key of the nested object
-      else if (answer === "cd") {
-        rl.question("Enter the key of the nested object: ", (key) => {
-          // If the key exists and is an object, call the prompt function recursively with the new path and object
-          if (obj[key] && typeof obj[key] === "object") {
-            prompt(`${path}.${key}`, obj[key]);
-          }
-          // Otherwise, print an error message and repeat the prompt
-          else {
-            console.error("Invalid key or not an object.");
-            prompt(path, obj);
-          }
-        });
-      }
-      // If the user enters a valid key, ask them for the new value
-      else if (obj.hasOwnProperty(answer)) {
-        rl.question("Enter the new value: ", (value) => {
-          // Try to parse the value as JSON, or use it as a string
-          try {
-            obj[answer] = JSON.parse(value);
-          } catch (err) {
-            obj[answer] = value;
-          }
-          // Repeat the prompt with the updated object
-          prompt(path, obj);
-        });
+  let path = []
+  while (true) {
+    let obj = payload
+    for (const prop of path){
+      obj = obj[prop]
+    }
+    console.log(obj)
+    const answer = await getInput('Enter a key to change its value, or \'cd\' to navigate to a nested object, ' +
+      ',\'save\' to save and exit, or \'back\' to navigate out of a nested object:')
+    // If the user enters 'save', encode the payload and print the new token
+    if (answer === "save") {
+      const newToken = jwt.sign(payload, "secret");
+      console.log(`New JWT token: ${newToken}`);
+      return
+    }
+    // If the user enters 'cd', ask them for the key of the nested object
+    else if (answer === "cd") {
+      const key = await getInput("Enter the key of the nested object: ")
+      // If the key exists and is an object, call the prompt function recursively with the new path and object
+      if (obj[key] && typeof obj[key] === "object") {
+        path.push(key)
       }
       // Otherwise, print an error message and repeat the prompt
       else {
-        console.error("Invalid key.");
-        prompt(path, obj);
+        console.error("Invalid key or not an object.");
       }
     }
-  );
-};
+    // If the user enters a valid key, ask them for the new value
+    else if (answer==='set'){
+      const key = await getInput('Enter the key to set: ')
+      if (obj.hasOwnProperty(key)) {
+        const value = await getInput("Enter the new value: ")
+        // Try to parse the value as JSON, or use it as a string
+        try {
+          obj[key] = JSON.parse(value);
+        } catch (err) {
+          obj[key] = value;
+        }
+      }else {
+        console.error("Invalid key.");
+      }
+    }
+     else if (answer === 'back') {
+      path.pop()
+    }
+    // Otherwise, print an error message and repeat the prompt
+    else {
+      console.error("Invalid syntax.");
+    }
+  }
+})()
 
-// Start the prompt with the root path and payload
-prompt("payload", payload);
--- website/server.js
+++ website/server.js
@@ -1,33 +1,41 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const jwt  = require('jsonwebtoken')
+const jwt = require('jsonwebtoken')
 const cors = require('cors')
 const fs = require('fs')
 
 app.use(cors());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({extended: true}));
 
-app.use(bodyParser.json({limit: '10mb',extended:true}));
+app.use(bodyParser.json({limit: '10mb', extended: true}));
 
-app.post('/login',async (req,res)=>{
-  console.log(req.body)
-  if (req.body.password === 'jimmy') {
-    		console.log(jwt.sign({username: req.body.username}, 'readthisfromfilelater'))
-	  res.json(jwt.sign({username: req.body.username}, 'readthisfromfilelater'))
+app.post('/login', async (req, res) => {
+  try {
+    console.log(req.body)
+    if (req.body.password === 'jimmy') {
+      console.log(jwt.sign({username: req.body.username}, 'readthisfromfilelater'))
+      res.json(jwt.sign({username: req.body.username}, 'readthisfromfilelater'))
+    }
+  } catch {
   }
 })
 
-app.post('/getFlag',async (req,res)=>{
-  const verified = jwt.verify(req.body.token,'readthisfromfilelater',{
-    algorithms:['HS256','HS384','HS512','RS256','RS384','none']
-  })
-  if (verified.username==='jimmy'){
-    res.json('flag1: ' + fs.readFileSync('../flags/flag1'))
-  }
-  if (verified.username==='administrator') {
-    res.json('flag2: ' + fs.readFileSync('../flags/flag2'))
+app.post('/getFlag', async (req, res) => {
+  try {
+    const verified = jwt.verify(req.body.token, 'readthisfromfilelater', {
+      algorithms: ['HS256', 'HS384', 'HS512', 'RS256', 'RS384', 'none']
+    })
+    if (verified.username === 'jimmy') {
+      res.json('flag1: ' + fs.readFileSync('../flags/flag1'))
+    }
+    if (verified.username === 'administrator') {
+      res.json('flag2: ' + fs.readFileSync('../flags/flag2'))
+    }
+  } catch {
   }
 })
 
-app.listen(8080,()=>{console.log('rdy')})
+app.listen(8080, () => {
+  console.log('rdy')
+})
